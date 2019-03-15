6e26a69f3bfda6d9bde5c087b68aecf825c9c523
Charles Blackburn <stormchaseruk@gmail.com>
2019-03-14 21:21:16 -0400

    Added comments to scripts

--- src/servocontrol.py
+++ src/servocontrol.py
@@ -1,9 +1,15 @@
+# Pololu Maestro Servo class
+# baed on the work by martin saint: http://martinsant.net/?page_id=479
+#
 import serial
 import time
 
 class ServoController:
 
-    def __init__(self):
+    #Class constructor
+    def __init__(self, usbPort):
+        # Set the lower, upper and range limits for each servo channel
+        # min ms, center ms, max ms, servo throw
         self.ServoLimits = [ [600, 1600, 2400, 270],
                             [1000, 1500, 2000, 180],
                             [1000, 1500, 2000, 180],
@@ -11,15 +17,20 @@ class ServoController:
                             [1000, 1500, 2000, 180],
                             [1000, 1500, 2000, 180]  ]
 
+        # Port that the maestro is connected to = usually TTYACM0/1
+        self.sc = serial.Serial(self.usbPort, timeout=1)
 
-        usbPort = '/dev/ttyACM0'
-        self.sc = serial.Serial(usbPort, timeout=1)
-
+    # Class Destructor
     def closeServo(self):
         self.sc.close()
 
     # Set an angle between -90 (full min to +90 full max, with 0 degrees as center position
+    # This checks the servo limits above to make sure it doesn't go past those
+    # converts the range that the servo can go so that it is always -90 to +90 degrees
+    # with 0 degrees being "center". 
+    # for example, a -90 on a 270 degree servo is always going to be the max the servo can go.
     def setAngle(self, chan, angle):
+        # Check limits
         if angle == 0:
             ms = self.ServoLimits[chan][1]
         elif angle == -90:
@@ -38,17 +49,21 @@ class ServoController:
     
             ms = (((angle - old_min) * NewRange) / OldRange) + new_min
 
+        # Set the position according to the calculated ms
         print "Servo ", chan, " to ", angle, " (", ms, ")"
         self.setPosition(chan, ms)
 
 
     # set definitive angle via maestro
+    # Alternative to above if you are using standard 180 degree servos
+    # this is the direct controlled version via pololu commands
     def setDefAngle(self, n, angle):
         if angle > 180 or angle <0:
            angle=90
         bud=chr(0xFF)+chr(n)+chr(byteone)
         self.sc.write(bud)
 
+    # Set the servo channel to a specified ms value
     def setPosition(self, servo, microsecs):
         microsecs = microsecs * 4
         poslo = (microsecs & 0x7f)
@@ -57,6 +72,7 @@ class ServoController:
         data =  chr(0xaa) + chr(0x0c) + chr(0x04) + chr(chan) + chr(poslo) + chr(poshi)
         self.sc.write(data)
 
+    # get the current position of the servo as a hi/lo nibble pair
     def getPosition(self, servo):
         chan  = servo &0x7f
         data =  chr(0xaa) + chr(0x0c) + chr(0x10) + chr(chan)
@@ -65,6 +81,7 @@ class ServoController:
         w2 = ord(self.sc.read())
         return w1, w2
 
+    # Get error codes as a hi/lo nibble pair
     def getErrors(self):
         data =  chr(0xaa) + chr(0x0c) + chr(0x21)
         self.sc.write(data)
@@ -72,6 +89,7 @@ class ServoController:
         w2 = ord(self.sc.read())
         return w1, w2
 
+    # Trigger a specified subrouting stored in the maestro
     def triggerScript(self, subNumber):
         data =  chr(0xaa) + chr(0x0c) + chr(0x27) + chr(0)
         self.sc.write(data)
--- src/test.py
+++ src/test.py
@@ -1,17 +1,26 @@
 import time
 import servocontrol
 
+# Create the servo controller
 print("Creating servo object")
 servos = servocontrol.ServoController()
 
+# Make a list of random bearing angles
 bearings=[-69,    -74,        38,        -70,        -2,        -18,        87,        -19,        88,        -44,        -85,        69,
             56,      48,        -71,        -8,        11,        -54,        77,        20,        -3,        8,        71,
             80,      66,        -9,        -59,        22,        30,        -78,        39,        85,        -15,        13,
             -16,    72,        -40,        -63,        -41,        -7,        64,        75,        68,        -12 ]
 
+
 numelements = len(bearings)
+
+# Set through the list and move the servo to specified angle using the servo limited angle function
 for i in bearings:
     servos.setAngle(0, i)
     time.sleep(0.25)
 
+# Reset servo position to center/home
 servos.setAngle(0, 0)
+
+# close the controller
+servos.closeServo()
